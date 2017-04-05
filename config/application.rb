c6cc01ed604ab572098ff368c861284279b131bd
Akash Devaraju <akash@digifutura.com>
2017-04-05 12:27:23 +0530

    [Database] Added UUID schema and migrations

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Webapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Default UUID migrations
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
